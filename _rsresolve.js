9e12a159e619511d0e8d73c18274adcbddaf1d76
Anon <anon@mailinator.com>
2011-10-26 12:49:17 +0200

    Improve rs tools

--- _rsresolve.js
+++ _rsresolve.js
@@ -12,10 +12,22 @@ function run(rsURL) {
                 var url = page.evaluate(function () {
                     return document.getElementById('js_dlpage_dlbtn').href;
                 });
-                console.log(url)
-                phantom.exit();
+                if (url != null && url.indexOf('http') == 0) {
+                    console.log(url);
+                    phantom.exit();
+                } else {
+                    var err = page.evaluate(function () {
+                        return document.getElementById('js_downloaderror').innerHTML;
+                    });
+                    if (err === null || err === "") {
+                        setTimeout(f, 100);
+                    } else {
+                        console.log(err);
+                        phantom.exit(1);
+                    }
+                }
             }
-            setTimeout(f, 1000);
+            setTimeout(f, 500);
         }
     }
 
@@ -35,5 +47,5 @@ if (phantom.args.length === 1) {
     run(phantom.args[0]);
 } else {
     console.log('usage: ' + phantom.scriptName + ' <URL>');
-    phantom.exit();
+    phantom.exit(1);
 }
--- download.sh
+++ download.sh
@@ -3,6 +3,12 @@
 BASE_DIR=$(dirname $0)
 
 URL=$("$BASE_DIR"/_rsresolve.js $1)
+
+if [ "$?" -ne "0" ]; then
+  echo "$URL"
+  exit 1
+fi
+
 FILENAME=$("$BASE_DIR"/_extract_filename.py $URL)
 
 wget -c -O "$FILENAME" "$URL"
