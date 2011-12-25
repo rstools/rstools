90a757b053f7c62906686baf41109b2e638c5bdd
Anon <anon@mailinator.com>
2011-12-25 18:08:26 +0100

    add a dispatching frontend

--- download.sh
+++ download.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-BASE_DIR=$(dirname $0)
-
-URL=$("$BASE_DIR"/_rsresolve.js $1)
-
-if [ "$?" -ne "0" ]; then
-  echo "$URL"
-  exit 1
+if [ $# -lt 1 ]
+then
+  echo "usage: $0 <url>"
 fi
 
-FILENAME=$("$BASE_DIR"/_extract_filename.py $URL)
+PAGEURL="$1"
+BASE_DIR=`dirname "$0"`
 
-wget -c -O "$FILENAME" "$URL"
+case "$PAGEURL" in
+*rapidshare*)
+  FILEURL=$("$BASE_DIR"/_rsresolve.js "$1")
+  if [ "$?" -ne "0" ]; then
+    echo "error: $PAGEURL"
+    exit 1
+  fi
+  FILENAME=$("$BASE_DIR"/_extract_filename.py "$FILEURL")
+  wget -c -O "$FILENAME" "$FILEURL"
+  ;;
+
+*mediafire*)
+  FILEURL=$("$BASE_DIR"/_mfresolve.js "$1")
+  if [ "$?" -ne "0" ]; then
+    echo "error: $PAGEURL"
+    exit 1
+  fi
+  wget -c "$FILEURL"
+  ;;
+
+*)
+  echo "unsupported hoster: $PAGEURL"
+  exit 1
+  ;;
+esac
